075a691e6eff0ee07d69338379447597d5cffd3c
Sivarak Harnvongsa <shiv4ruk@Sivaraks-MacBook-Pro.local>
2022-09-06 05:39:48 +0700

    test_bonus

---
+++ philo_bonus/philosopher_bonus.c
@@ -0,0 +1,264 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sharnvon <sharnvon@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 14:38:08 by sharnvon          #+#    #+#             */
+/*   Updated: 2022/09/06 05:38:09 by sharnvon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher_bonus.h"
+
+/* ./philo [number of philos] [time to die] [time to eat] [time to sleep] */
+	/* optional "number of time to eat" */
+void	ft_check_arguments(int argc, char **argv)
+{
+	int	index;
+	int	xedni;
+
+	index = 1;
+	if (argc < 5 || argc > 6)
+	{
+		printf("ERROR: THE NUMBER OF THE AGUMENT IS WRONG :(\n");
+		printf(" (hint) ./philo [number of philos] [time to die] [time to eat]"
+			" [time to sleep] [(optional): number of time to eat]\n");
+		exit(EXIT_FAILURE);
+	}
+	while (argv[index] != NULL)
+	{
+		xedni = 0;
+		while (argv[index][xedni] != '\0')
+		{
+			if (argv[index][xedni] < '0' || argv[index][xedni] > '9')
+			{
+				printf("ERROR: VALUE OF AGURMENTS MUST BE POSITIVE NUMBER..\n");
+				exit(EXIT_FAILURE);
+			}
+			xedni++;
+		}
+		index++;
+	}
+}
+
+/* function initialise the t_table structure */
+t_table	*ft_table_init(t_table *table, char **argv, int index)
+{
+	table = ft_calloc(sizeof(t_table), 1);
+	if (table == NULL)
+		return (0);
+	table->philo = (t_philo *)ft_calloc(sizeof(t_philo), ft_atoi(argv[1]));
+	if (table->philo == NULL)
+	{
+		free(table);
+		return (0);
+	}
+
+	table->pid = (int *)ft_calloc(sizeof(int), table->amount);
+	if (table->pid == NULL)
+	{
+		free(table->philo);
+		free(table);
+		return (0);
+	}
+	table->amount = ft_atoi(argv[1]);
+	table->eat = ft_atoi(argv[3]) * 1000;
+	table->sleep = ft_atoi(argv[4]) * 1000;
+	table->life_time = ft_atoi(argv[2]) * 1000;
+	table->philo_status = SLEEP;
+	if (argv[5] != NULL)
+		table->meal = ft_atoi(argv[5]);
+	else
+		table->meal = -1;
+	sem_unlink(SEMFORK); //
+	table->sem_fork = sem_open(SEMFORK, O_CREAT | O_EXCL, 0644, table->amount);
+	while (index < table->amount)
+	{
+	//	pthread_mutex_init(&table->philo[index].mutex_fork, NULL);
+	//	pthread_mutex_init(&table->philo[index].mutex_lifetime, NULL);
+		sem_unlink(ft_strjoin(SEMLIFE, ft_positive_itoa(index))); //
+		table->philo[index].sem_lifetime = sem_open(ft_strjoin(SEMLIFE, ft_positive_itoa(index)), O_CREAT | O_EXCL, 0644, 1);
+		table->philo[index++].life = ft_atoi(argv[2]) * 1000;
+	}
+	return (table);
+}
+/*
+void	ft_create_philo(t_table *table, int amount, int count)
+{
+	while (count + 1 < amount)
+	{
+		if (pthread_create(&table->philo[count].thread, NULL, \
+			&ft_philo_routine, (void *)table) != 0)
+			ft_free_resource(table, FAILURE);
+		if (count + 2 != table->amount)
+			count += 2;
+		else
+			count++;
+	}
+	if (amount == 1 && count == 0)
+	{
+		if (pthread_create(&table->philo[count].thread, NULL, \
+			&ft_philo_routine, (void *)table) != 0)
+			ft_free_resource(table, FAILURE);
+	}
+}
+*/
+/* ./philo [number of philos] [time to die] [time to eat] [time to sleep] */
+	/* optional "number of time to eat" */
+int	main(int argc, char **argv)
+{
+	t_table		*table;
+	pthread_t	thread;
+	int			count;
+//	int			*pid;
+
+	count = 0;
+	ft_check_arguments(argc, argv);
+	table = ft_table_init(NULL, argv, 0);
+	if (table == NULL)
+		return (EXIT_FAILURE);
+
+	// malloc pid use for kill the process //
+	////////////////////////////////////////
+	
+	
+	// chai mai dai // not working //
+	if (argv[5] != NULL)
+	{
+		if (pthread_create(&table->thread, NULL, &ft_philo_mealcount, (void *)table) != 0)
+			ft_free_resource(table, FAILURE);
+	}
+	/////////////////////////////////////////
+
+
+
+	// philo_funtion //
+	
+	while (count < table->amount)
+	{
+		table->pid[count] = fork();
+		if (table->pid[count] < 0)
+		{
+			// need to kill another child process //
+			while (count >= 0)
+				kill(table->pid[count--], SIGINT);
+			ft_free_resource(table, FAILURE);
+		}
+		if (table->pid[count] == 0)
+			break ;
+		count++;
+	}
+	if (count < table->amount && table->pid[count] == 0)
+	{
+		//printf("(child)count = %d\n", count);
+		table->count = count;
+		pthread_create(&thread, NULL, &ft_philo_lifetime, (void *)table);
+		while (table->philo_status == SLEEP);
+	//	pthread_create(&table->philo[count].thread, NULL, &ft_philo_routine, (void *)table);
+		if (count > table->amount / 2)
+			ft_isleepnow(table->eat);
+		while (1)//(table->philo_status != DEAD && table->philo_status != FULL)
+		{
+			sem_wait(table->sem_fork);
+			ft_philo_printf(table, count + 1, FORK_TAKEN);
+			sem_wait(table->sem_fork);
+			ft_philo_printf(table, count + 1, FORK_TAKEN);
+			ft_philo_printf(table, count + 1, EAT);
+			ft_isleepnow(table->eat);
+			sem_wait(table->philo[count].sem_lifetime);
+			table->philo[count].life = table->life_time;
+			sem_post(table->philo[count].sem_lifetime);
+			table->philo[count].meal++;
+			sem_post(table->sem_fork);
+			sem_post(table->sem_fork);
+			ft_philo_printf(table, count + 1, SLEEP);
+			ft_isleepnow(table->sleep);
+			ft_philo_printf(table, count + 1, THINK);
+		}
+		pthread_join(thread, NULL);
+		printf("(child)count == %d, pid[%d] = %d\n", count, count, table->pid[count]);
+	}
+	else
+	{
+		while (waitpid(-1, NULL, 0) != -1 || errno != ECHILD)
+		{
+			while (--count >= 0)
+				kill(table->pid[count], SIGINT);
+		}
+		printf("(parent):end\n");
+		ft_free_resource(table, SUCCESS);
+//		while(1);
+	//	printf("HI-LOW O_OKAY\n");
+	}
+//	printf("end\n");
+	return (EXIT_SUCCESS);
+}
+/*
+void	*ft_philo_routine(void *elbat)
+{
+	t_table		*table;
+//	static int	number = 0;
+//	int			count;
+
+	table = (t_table *)elbat;
+	count = number++;
+	printf("count = %d\n", count);
+	if (count > table->amount / 2)
+		ft_isleepnow(table->eat);
+	while (table->philo_status != DEAD && table->philo_status != FULL)
+	{
+		sem_wait(table->sem_fork);
+		ft_philo_printf(table, count + 1, FORK_TAKEN);
+		sem_wait(table->sem_fork);
+		ft_philo_printf(table, count + 1, FORK_TAKEN);
+		ft_philo_printf(table, count + 1, EAT);
+		ft_isleepnow(table->eat);
+		printf("phio[%d].liffe == %lu\n", count, table->philo[count].life);
+		sem_wait(table->philo[count].sem_lifetime);
+		table->philo[count].life = table->life_time;
+		sem_post(table->philo[count].sem_lifetime);
+		table->philo[count].meal++;
+		sem_post(table->sem_fork);
+		sem_post(table->sem_fork);
+		ft_philo_printf(table, count + 1, SLEEP);
+		ft_isleepnow(table->sleep);
+		ft_philo_printf(table, count + 1, THINK);
+		printf("phio[%d].life == %lu\n", count, table->philo[count].life);
+	}
+	return (0);
+}*/
+
+// free pid in ft_free_resource //
+// 
+
+/*
+int	main(int argc, char **argv)
+{
+	t_table		*table;
+	pthread_t	thread;
+
+	ft_check_arguments(argc, argv);
+	table = ft_table_init(NULL, argv, 0);
+	if (table == NULL)
+		return (EXIT_FAILURE);
+	if (argv[5] != NULL)
+	{
+		if (pthread_create(&table->thread, NULL, \
+			&ft_philo_mealcount, (void *)table) != 0)
+			ft_free_resource(table, FAILURE);
+	}
+	ft_create_philo(table, table->amount, 0);
+	if (pthread_create(&thread, NULL, &ft_philo_lifetime, (void *)table) != 0)
+		ft_free_resource(table, FAILURE);
+	while (table->philo_status == SLEEP)
+	{
+	}
+	ft_isleepnow(table->eat);
+	ft_create_philo(table, table->amount + 1, 1);
+	if (pthread_join(thread, NULL) != 0)
+		ft_free_resource(table, FAILURE);
+	ft_free_resource(table, SUCCESS);
+	return (EXIT_SUCCESS);
+}*/
---
+++ philo_bonus/philosopher_thread_bonus.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher_thread_bonus.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sharnvon <sharnvon@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 16:07:42 by sharnvon          #+#    #+#             */
+/*   Updated: 2022/09/06 05:37:21 by sharnvon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher_bonus.h"
+
+/*
+void	*ft_philo_routine(void *elbat)
+{
+	t_table		*table;
+	int			i;
+
+	table = (t_table *)elbat;
+	i = ft_get_index(table);
+	while (1 > 0)
+	{
+		pthread_mutex_lock(&table->philo[i].mutex_fork);
+		ft_philo_printf(table, i + 1, FORK_TAKEN);
+		pthread_mutex_lock(&table->philo[(i + 1) % table->amount].mutex_fork);
+		ft_philo_printf(table, i + 1, FORK_TAKEN);
+		ft_philo_printf(table, i + 1, EAT);
+		ft_isleepnow(table->eat);
+		pthread_mutex_lock(&table->philo[i].mutex_lifetime);
+		table->philo[i].life = table->life_time;
+		pthread_mutex_unlock(&table->philo[i].mutex_lifetime);
+		table->philo[i].meal++;
+		pthread_mutex_unlock(&table->philo[i].mutex_fork);
+		pthread_mutex_unlock(&table->philo[(i + 1) % table->amount].mutex_fork);
+		ft_philo_printf(table, i + 1, SLEEP);
+		ft_isleepnow(table->sleep);
+		ft_philo_printf(table, i + 1, THINK);
+	}
+}
+
+int	ft_get_index(t_table *table)
+{
+	static int	count = 0;
+	int			index;
+
+	if (count + 1 > table->amount)
+		count = 1;
+	index = count;
+	if (count + 2 != table->amount)
+		count += 2;
+	else
+		count++;
+	while (table->philo_status == SLEEP)
+	{
+	}
+	return (index);
+}
+*/
+void	*ft_philo_mealcount(void *elbat)
+{
+	int		count;
+	int		index;
+	t_table	*table;
+
+	table = (t_table *)elbat;
+	count = 0;
+	while (count != table->amount)
+	{
+		index = 0;
+		count = 0;
+		while (index < table->amount)
+		{
+		//	printf("philo[%d].meal == %d\n", index, table->philo[index].meal);
+			if (table->philo[index].meal >= table->meal)
+				count++;
+			index++;
+		}
+	}
+	table->philo_status = FULL;
+	return (0);
+}
+
+void	*ft_philo_lifetime(void *elbat)
+{
+//	int	i;
+	t_table	*table;
+
+	table = (t_table *)elbat;
+	ft_timestamp_cal((t_table *)table, TIME_REGIS);
+	while (0 < 1)
+	{
+		ft_isleepnow(1000);
+//		i = -1;
+//		while (++i < ((t_table *)table)->amount)
+//		{
+		//	pthread_mutex_lock(&((t_table *)table)->philo[i].mutex_lifetime);
+			sem_wait(((t_table *)table)->philo[table->count].sem_lifetime);
+			((t_table *)table)->philo[table->count].life -= 1000;
+			sem_post(((t_table *)table)->philo[table->count].sem_lifetime);
+		//	pthread_mutex_unlock(&((t_table *)table)->philo[i].mutex_lifetime);
+			if (((t_table *)table)->philo[table->count].life <= 0)
+			{
+			//	((t_table *)table)->philo_status = DEAD;
+				ft_philo_printf(((t_table *)table), table->count + 1, DEAD);
+//				printf("(lifetime)kill) count = %d\n", table->count);
+//				kill(table->pid[table->count - 1], SIGINT);
+				exit(0);
+			}
+			if (((t_table *)table)->philo_status == FULL)
+				return (0);
+//		}
+	}
+	return (0);
+}
+
+void	ft_philo_printf(t_table *table, int index, int mode)
+{
+	if (mode == FORK_TAKEN && table->philo_status == ALIVE)
+	{
+		printf("=> \033[0;33m%lu\033[0m ms, \033[0;36m%d\033[0m has taken"
+			" a fork.\n", ft_timestamp_cal(table, TIME_CAL), index);
+	}
+	else if (mode == EAT && table->philo_status == ALIVE)
+	{
+		printf("=> \033[0;33m%lu\033[0m ms, \033[0;36m%d\033[0m is eating.\n",
+			ft_timestamp_cal(table, TIME_CAL), index);
+	}
+	else if (mode == SLEEP && table->philo_status == ALIVE)
+	{
+		printf("=> \033[0;33m%lu\033[0m ms, \033[0;36m%d\033[0m is sleeping.\n",
+			ft_timestamp_cal(table, TIME_CAL), index);
+	}
+	else if (mode == THINK && table->philo_status == ALIVE)
+	{
+		printf("=> \033[0;33m%lu\033[0m ms, \033[0;36m%d\033[0m is thinking.\n",
+			ft_timestamp_cal(table, TIME_CAL), index);
+	}
+	else if (mode == DEAD)
+	{
+		printf("=> \033[0;33m%lu\033[0m ms, \033[0;36m%d\033[0m \033[0;31mdied."
+			"\033[0m\n", ft_timestamp_cal(table, TIME_CAL), index);
+	}
+}
